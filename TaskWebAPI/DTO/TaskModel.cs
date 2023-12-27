b6f7fe2a2009f8007168e4cf2df39c228c196dfb
Prasad Sharad Adsul <PRASADADSUL81@GMAIL.COM>
2023-12-27 20:05:09 +0530

    Assignment 4_Web API Task_Management

---
+++ TaskWebAPI/DTO/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TaskWebAPI.DTO
+{
+    public class TaskModel
+    {
+        [JsonProperty(PropertyName = "taskName", NullValueHandling = NullValueHandling.Ignore)]
+        public string TaskName { get; set; }
+
+        [JsonProperty(PropertyName = "taskDescription", NullValueHandling = NullValueHandling.Ignore)]
+        public string TaskDescription { get; set; }
+    }
+}
